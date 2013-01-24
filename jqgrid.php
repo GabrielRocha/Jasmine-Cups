bc9a53c6698fa5e09ca5f0bbbdfba1dbb788d107
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2013-01-24 15:07:21 -0200

    Update SQL

--- jqgrid.php
+++ jqgrid.php
@@ -4,18 +4,12 @@ include_once("jasConfig.php");
 
 $page = $_GET['page'];
 $limit = (int)$_GET['rows'];
-$sidx = $_GET['sidx'];
-$sord = $_GET['sord'];
 $user = $_GET['user'];
 $printer = $_GET['printer'];
 $host = $_GET["host"];
 
 $column_order = "date";
 $order = "desc";
-
-if(!$sidx) $sidx =1;
-//$db = mysql_connect($DB_host, $DB_login, $DB_pass) or die("Connection Error: " . mysql_error());
-//mysql_select_db($DB_db) or die("Error connecting to db.");
 
 $opcoes = array(
     PDO::ATTR_PERSISTENT => true,
@@ -32,10 +26,8 @@ if (isset($user)){
 	$result = $dbh->prepare("SELECT COUNT(*) AS count FROM jobs_log WHERE user=:user");
 	$result->bindValue(':user', $user);
 	make_info_pages_jqgrid();
-	$result = $dbh->prepare("Select id,date,title, host, server, printer,copies,pages FROM jobs_log WHERE user=:user ORDER BY $column_order $order LIMIT $start,$limit");
+	$result = $dbh->prepare("Select id, DATE_FORMAT(date, '%d/%m/%Y %H:%i:%s') as date ,title, host, server, printer,copies,pages FROM jobs_log WHERE user=:user ORDER BY $column_order $order LIMIT $start,$limit");
 	$result->bindValue(':user', $user);
-	// $result->bindValue(':sidx', $sidx);
-	// $result->bindValue(':sord', $sord);
 	if ($result->execute()) {
 		$i=0;
 		foreach($result as $row) {
@@ -50,10 +42,8 @@ if (isset($user)){
 	$result = $dbh->prepare("SELECT COUNT(*) AS count FROM jobs_log WHERE printer=:printer");
 	$result->bindValue(':printer', $printer);
 	make_info_pages_jqgrid();
-	$result = $dbh->prepare("Select id,date,title, host, server, user,copies,pages FROM jobs_log WHERE printer=:printer ORDER BY $column_order $order LIMIT $start,$limit");
+	$result = $dbh->prepare("Select id,DATE_FORMAT(date, '%d/%m/%Y %H:%i:%s') as date,title, host, server, user,copies,pages FROM jobs_log WHERE printer=:printer ORDER BY $column_order $order LIMIT $start,$limit");
 	$result->bindValue(':printer', $printer);
-	// $result->bindValue(':sidx', $sidx);
-	// $result->bindValue(':sord', $sord);
 	if ($result->execute()) {
 		$i=0;
 		foreach($result as $row) {
@@ -68,10 +58,8 @@ if (isset($user)){
 	$result = $dbh->prepare("SELECT COUNT(*) AS count FROM jobs_log WHERE host=:host");
 	$result->bindValue(':host', $host);
 	make_info_pages_jqgrid();
-	$result = $dbh->prepare("Select id,date,title, printer, server, user,copies,pages FROM jobs_log WHERE host=:host  ORDER BY $column_order $order 	LIMIT $start, $limit");
+	$result = $dbh->prepare("Select id,DATE_FORMAT(date, '%d/%m/%Y %H:%i:%s') as date,title, printer, server, user,copies,pages FROM jobs_log WHERE host=:host  ORDER BY $column_order $order 	LIMIT $start, $limit");
 	$result->bindValue(':host', $host);
-	// $result->bindValue(":sidx", $sidx,  PDO::PARAM_STR);
-	// $result->bindValue(':sord', $sord, PDO::PARAM_STR);
 	if ($result->execute()) {
 		$i=0;
 	 	foreach($result as $row) {
